54c6fe7e948c4c96ceefab04bf900dcf838e2057
emifa <emifa@LAPTOP-1TM449QB>
2023-06-28 16:12:06 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-1- Data_Coll .NET/PrintList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_1__Data_Coll_.NET
+{
+    public class PrintList
+    {
+       public List<string> Print(List<string>ls)
+        {
+            for (int i = 0; ls.Count <= 10; i++)
+            {
+                Console.WriteLine("Insert input");
+                var inp = Console.ReadLine();
+                ls.Add(inp);
+                
+            }
+
+            ls.ForEach(Console.WriteLine);
+            return ls;
+        }
+
+        public List<string> PrintReverse(List<string>ls)
+        {
+            ls.Reverse();
+            string concat = string.Join(", ", ls);
+            Console.WriteLine(concat);
+            return ls;  
+        }
+
+       
+    }
+}
---
+++ Esercizio-1- Data_Coll .NET/Program.cs
@@ -0,0 +1,6 @@
+﻿using Esercizio_1__Data_Coll_.NET;
+
+List<string> list = new List<string>();
+PrintList pl=new PrintList();
+pl.Print(list);
+pl.PrintReverse(list);  
